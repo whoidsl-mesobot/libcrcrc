0bf90c7621a4b6ed63913a5cbe31e376add4da60
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2019-04-21 18:56:14 -0700

    add vesc_duty_cycle test, and convenience methods for printing

--- src/libcrcrc.c
+++ src/libcrcrc.c
@@ -1,5 +1,8 @@
 #include <arpa/inet.h>
 
+#include <stdio.h>
+#include <inttypes.h>
+
 #include "libcrcrc.h"
 
 // decide whether to expose all methods and install all headers, or just the
@@ -7,9 +10,32 @@
 #include "crc16xmodem.h"
 #include "crc16ccitt_false.h"
 
+// for sanity...
+int crc16_fprintf( FILE * stream, uint16_t crc ) {
+    return fprintf( stream, "0x%04" PRIx16, crc );
+}
+
+
+int crc16_calculate_fprintf( FILE * stream,
+        void * crc16( const void * data, size_t length ),
+        const void * data, size_t length ) {
+    uint16_t crc = crc16( data, length );
+    int n = 0;
+    n += fprintf( stream, "%zub data, crc16( ", length );
+    for( int j = 0; j < length; j++ ) {
+        n += fprintf( stderr, "%02" PRIx8 " ", ((uint8_t *)data)[j] );
+    }
+    n += fprintf( stream, ") = " );
+    n += crc16_fprintf( stream, crc );
+    n += fprintf( stream, "\n" );
+    return n;
+}
+
+
 uint16_t crc16xmodem( const void * data, size_t length ) {
     return htons( crc16xmodem_byte( 0, data, length ) );
 }
+
 
 uint16_t crc16ccitt_false( const void * data, size_t length ) {
     return htons( crc16ccitt_false_byte( 0xFFFF, data, length ) );
--- test/test-libcrcrc.c
+++ test/test-libcrcrc.c
@@ -8,10 +8,6 @@
 
 #include "libcrcrc.h"
 
-// for sanity's sake
-static void crc_printf( uint16_t crc ) {
-    printf( "crc: %04" PRIx16 "\n", crc );
-}
 
 
 // Xmodem 0x04 --> 0x4084
@@ -21,17 +17,37 @@ static int test_crc16xmodem_04( void ) {
     uint16_t expected = htons( 0x4084 );
     uint16_t crc = 0;
     crc = crc16xmodem( &data, 1 );
+    crc16_calculate_fprintf( stderr, &crc16xmodem, &data, 1 );
     assert( crc == expected );
     return EXIT_SUCCESS;
 }
 
 static int test_crc16xmodem_04_inclusive( void ) {
-    const char data[3] = { '\x04' ,'\x40' , '\x84' };
+    const char data[3] = { '\x04' ,'\x40', '\x84' };
     uint16_t crc = crc16xmodem( data, 3 );
+    crc16_calculate_fprintf( stderr, &crc16xmodem, data, 3 );
     assert( crc == 0 );
     return EXIT_SUCCESS;
 }
 
+
+static int test_crc16xmodem_vesc_duty_cycle( void ) {
+    const char data[5] = { 0x05, 0x00, 0x01, 0x86, 0xA0 };
+    uint16_t expected = htons( 0x10B3 );
+    uint16_t crc = 0;
+    crc = crc16xmodem( data, 5 );
+    crc16_calculate_fprintf( stderr, &crc16xmodem, data, 5 );
+    assert( crc == expected );
+    return EXIT_SUCCESS;
+}
+
+static int test_crc16xmodem_vesc_duty_cycle_inclusive( void ) {
+    const char data[7] = { 0x05, 0x00, 0x01, 0x86, 0xA0, 0x10, 0xB3 };
+    uint16_t crc = crc16xmodem( data, 7 );
+    crc16_calculate_fprintf( stderr, &crc16xmodem, data, 7 );
+    assert( crc == 0 );
+    return EXIT_SUCCESS;
+}
 
 
 static int test_crc16ccitt_false( void ) {
@@ -44,7 +60,7 @@ static int test_crc16ccitt_false( void ) {
 }
 
 static int test_crc16ccitt_false_inclusive( void ) {
-    const char data[3] = { '\x03' ,'\xD1' , '\x93' };
+    const char data[3] = { '\x03' ,'\xD1', '\x93' };
     uint16_t crc = crc16ccitt_false( data, 3 );
     assert( crc == 0 );
     return EXIT_SUCCESS;
@@ -55,6 +71,8 @@ int main( void ) {
     int status = EXIT_SUCCESS;
     status &= test_crc16xmodem_04();
     status &= test_crc16xmodem_04_inclusive();
+    status &= test_crc16xmodem_vesc_duty_cycle();
+    status &= test_crc16xmodem_vesc_duty_cycle_inclusive();
     status &= test_crc16ccitt_false();
     status &= test_crc16ccitt_false_inclusive();
     return status;
